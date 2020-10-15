6741c5c57d85064358f9b5e1d3283848bcd6445a
Joelopl <omplou.joel@yahoo.fr>
2020-10-15 15:33:39 +0200

    myreact

--- ls
+++
@@ -1,5 +0,0 @@
-[33mcommit 18f93388b142a565ebbc988463156281ed6effff[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
-Author: Joelopl <omplou.joel@yahoo.fr>
-Date:   Thu Oct 15 14:56:58 2020 +0200
-
-    my-react
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,10 @@
-/ Dependencies
+// Dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-const App = () => <div></div>;
+const App = () => (
+  <div>
+    <h1>Hello</h1>
+  </div>
+);
 ReactDOM.render(<App />, document.getElementById('app'));
