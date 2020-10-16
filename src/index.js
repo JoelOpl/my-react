b9d987cabf41711f136eebe849018f6c16fd2de4
Joelopl <omplou.joel@yahoo.fr>
2020-10-16 10:57:49 +0200

    modifié

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-// Dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import NavBar from './components/navbar';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
-const App = () => <div></div>;
+const App = () => (
+  <div>
+    <NavBar />
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('app'));
